31a1d653de622a5064942297b0ea34b48b60b578
harsha jayaweera <harshapl400soadxxinnovators@gmail.com>
2016-12-03 18:11:02 +0530

    bug fix commit

--- js/google-map-init.js
+++ js/google-map-init.js
@@ -135,3 +135,143 @@ jQuery(document).ready(function() {
     google.maps.event.addDomListener(window, "load", b);
 
 });
+
+
+function() {
+    "use strict";
+
+
+    function initMap() {
+        console.log("init called")
+        var a = {
+                zoom: 13,
+                scrollwheel: false,
+                center: new google.maps.LatLng(6.895233, 79.856657),
+                styles: [{
+                    "featureType": "landscape",
+                    "stylers": [
+                        {
+                            "saturation": -100
+                        },
+                        {
+                            "lightness": 65
+                        },
+                        {
+                            "visibility": "on"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "stylers": [
+                        {
+                            "saturation": -100
+                        },
+                        {
+                            "lightness": 51
+                        },
+                        {
+                            "visibility": "simplified"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "stylers": [
+                        {
+                            "saturation": -100
+                        },
+                        {
+                            "visibility": "simplified"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.arterial",
+                    "stylers": [
+                        {
+                            "saturation": -100
+                        },
+                        {
+                            "lightness": 30
+                        },
+                        {
+                            "visibility": "on"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.local",
+                    "stylers": [
+                        {
+                            "saturation": -100
+                        },
+                        {
+                            "lightness": 40
+                        },
+                        {
+                            "visibility": "on"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit",
+                    "stylers": [
+                        {
+                            "saturation": -100
+                        },
+                        {
+                            "visibility": "simplified"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "administrative.province",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "elementType": "labels",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        },
+                        {
+                            "lightness": -25
+                        },
+                        {
+                            "saturation": -100
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "hue": "#ffff00"
+                        },
+                        {
+                            "lightness": -25
+                        },
+                        {
+                            "saturation": -97
+                        }
+                    ]
+                }]
+            },
+            b = document.getElementById("map"),
+            c = new google.maps.Map(b, a);
+        new google.maps.Marker({
+            position: new google.maps.LatLng(6.895233, 79.856657),
+            map: c,
+            title: "Management Frontiers (Private) Limited"
+        })
+    }
+    google.maps.event.addDomListener(window, "load", b);
+
+}
