3bb555548a5555b28d07563f637dcd2026e515da
harsha jayaweera <harshapl400soadxxinnovators@gmail.com>
2016-12-04 14:11:31 +0530

    Added Client revisions.

--- js/google-map-init.js
+++ js/google-map-init.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function() {
         var a = {
                 zoom: 13,
                 scrollwheel: false,
-                center: new google.maps.LatLng(6.895233, 79.856657),
+                center: new google.maps.LatLng(6.9155129,79.8821746),
                 styles: [{
                     "featureType": "landscape",
                     "stylers": [
@@ -127,7 +127,7 @@ jQuery(document).ready(function() {
             b = document.getElementById("map"),
             c = new google.maps.Map(b, a);
         new google.maps.Marker({
-            position: new google.maps.LatLng(6.895233, 79.856657),
+            position: new google.maps.LatLng(6.9155129,79.8821746),
             map: c,
             title: "Management Frontiers (Private) Limited"
         })
@@ -135,139 +135,3 @@ jQuery(document).ready(function() {
     google.maps.event.addDomListener(window, "load", b);
 
 });
-
-
-
-
-    function initMap() {
-        console.log("init called")
-        var a = {
-                zoom: 13,
-                scrollwheel: false,
-                center: new google.maps.LatLng(6.895233, 79.856657),
-                styles: [{
-                    "featureType": "landscape",
-                    "stylers": [
-                        {
-                            "saturation": -100
-                        },
-                        {
-                            "lightness": 65
-                        },
-                        {
-                            "visibility": "on"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "poi",
-                    "stylers": [
-                        {
-                            "saturation": -100
-                        },
-                        {
-                            "lightness": 51
-                        },
-                        {
-                            "visibility": "simplified"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road.highway",
-                    "stylers": [
-                        {
-                            "saturation": -100
-                        },
-                        {
-                            "visibility": "simplified"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road.arterial",
-                    "stylers": [
-                        {
-                            "saturation": -100
-                        },
-                        {
-                            "lightness": 30
-                        },
-                        {
-                            "visibility": "on"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road.local",
-                    "stylers": [
-                        {
-                            "saturation": -100
-                        },
-                        {
-                            "lightness": 40
-                        },
-                        {
-                            "visibility": "on"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "transit",
-                    "stylers": [
-                        {
-                            "saturation": -100
-                        },
-                        {
-                            "visibility": "simplified"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "administrative.province",
-                    "stylers": [
-                        {
-                            "visibility": "off"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "water",
-                    "elementType": "labels",
-                    "stylers": [
-                        {
-                            "visibility": "on"
-                        },
-                        {
-                            "lightness": -25
-                        },
-                        {
-                            "saturation": -100
-                        }
-                    ]
-                },
-                {
-                    "featureType": "water",
-                    "elementType": "geometry",
-                    "stylers": [
-                        {
-                            "hue": "#ffff00"
-                        },
-                        {
-                            "lightness": -25
-                        },
-                        {
-                            "saturation": -97
-                        }
-                    ]
-                }]
-            },
-            b = document.getElementById("map"),
-            c = new google.maps.Map(b, a);
-        new google.maps.Marker({
-            position: new google.maps.LatLng(6.895233, 79.856657),
-            map: c,
-            title: "Management Frontiers (Private) Limited"
-        })
-    }
-
